2e3232bae81ec0e9ea8bc2869372bc6c9f040e8b
w4ba5h team <aboba@example.com>
2024-04-18 16:41:59 +0500

    changed everything + added some links

--- sns_osint.py
+++ sns_osint.py
@@ -1,13 +1,6 @@
-from selenium import webdriver
-
-browser_type = input("Choose browser: 1 - Chrome, 2 - Firefox: ")
-
-if browser_type == "1":
-	options = webdriver.ChromeOptions()
-	driver = webdriver.Chrome(options=options)
-else:
-	options = webdriver.FirefoxOptions()
-	browser = webdriver.Firefox(options=options)
+import webbrowser
+import requests
+import time
 
 domains = ["facebook.com", #/nickname
 "youtube.com", #/@nickname
@@ -30,23 +23,40 @@ domains = ["facebook.com", #/nickname
 "deviantart.com",
 "imgur.com"]
 
-slash_domains = ["facebook.com/", "instagram.com/", "twitter.com/", "twitch.tv/", "tumblr.com/", "vk.com/"] #problems with: "pinterest.com/",
+slash_domains = ["facebook.com/", "instagram.com/", "twitter.com/", "twitch.tv/", "tumblr.com/", "vk.com/", "shikimori.one/", "bit.ly/", "clck.ru/", "cutt.ly/"] #problems with: "pinterest.com/",
 at_domains = ["youtube.com/@", "tiktok.com/@", "threads.net/@", "mastodon.social/@", ]
-other_domains = ["snapchat.com/add/", "reddit.com/user/", "linkedin.com/in/",
+other_domains = ["snapchat.com/add/", "reddit.com/user/", #"linkedin.com/in/",
                   "quora.com/profile/", "discordlookup.com/user/", "bsky.app/profile/", "ok.ru/profile/", "deviantart.com/", "imgur.com/user/"]
 
 user_domains = slash_domains + at_domains + other_domains
+
+def check_website_content(url):
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url)
+        # Check if the HTTP status code is 200 (OK)
+        if response.status_code == 200:
+            return True
+        else:
+            print("Failed to retrieve content: Status code", response.status_code)
+            return False
+    except requests.RequestException as e:
+        print("An error occurred:", e)
+        return False
 
 mode = input("Choose mode: 1 - search by username, 2 - search by key word: ")
 
 if mode == "1":
     username = input("Input username to search on social networks: ")
-	
     for domain in user_domains:
-        browser.get("https://www." + domain + username)
-        browser.switch_to.new_window('tab')
-    
-else:
+        print(domain)
+        search_template = "https://www." + domain + username
+        if check_website_content(search_template):
+            webbrowser.open("https://www." + domain + username)
+        else:
+            print("Content does not exist!")
+            
+else: #TODO: fix this mode, bcs it can't find existing links from the mode 1
     keyword = input("Input keyword to search on social networks: ")
 
     searches = []
@@ -56,5 +66,5 @@ else:
         searches.append(search_template)
 
     for search_string in searches:
-        browser.get("https://www.google.com/search?q=" + search_string + "&start=" + str(search_string))
-        browser.switch_to.new_window('tab')
+        webbrowser.open("https://www.google.com/search?q=" + search_string + "&start=" + str(search_string))
+        time.sleep(1)
